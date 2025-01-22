a119188a86b23a66cc2c6383aab46652f248870f
Aniket Mondal <136544184+aniketmondal1210@users.noreply.github.com>
2025-01-22 11:32:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createHeart() {
     heart.style.width = `${size}px`;
     heart.style.height = `${size}px`;
     heart.style.left = `${left}%`;
-    heart.style.background = `rgba(255,${bg-25},${bg},1)`;
+    heart.style.background = `rgba(255,${bg - 25},${bg},1)`;
     heart.style.animation = `love ${time}s ease`;
 
     document.querySelector('.bg_heart').appendChild(heart);
@@ -26,16 +26,16 @@ function createHeart() {
 const interval = window.innerWidth < 480 ? 800 : 500;
 setInterval(createHeart, interval);
 
-const txt1 = "Hi SweetHeart.....!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time < You seems something Special to me.  <<                  As the days goes < you get closer to me....! <<                           I don't know the reason why your thoughts always resonates inside my mind...!                                                     > Everything about you is always intresting for me...!                     << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in future...!                                                     > I Love my Parents so much than anything else in this world....!                    << Now, you are the only person I love almost as much as them, yet in a way that's just yours alone....!                                                             >I Love U <SweetHeart.....!💕                 <<<< Give me One chance to Prove my Love ...!";
+const txt1 = "Hi Sweetheart ❤️.....!  <<               Now I want to say something special to you 💌. <<<                So, please read everything carefully...!                                                                           > When I saw you for the first time, < you seemed like someone special to me 🌟.  <<                  As the days went by, < you got closer to me....! <<                           I don't know the reason why your thoughts always resonate in my mind...! 🌀                                                     > Everything about you is always interesting to me...! 🥰                     << I am somewhat nervous because I haven't said these words to anyone, and I won't say them to anyone in the future...!                                                     > I love my parents more than anything else in this world 🏡❤️....!                    << Now you are the only person I love almost as much as them 🫂, yet in a way that's just yours alone....! 💕                                                             > I love you, < Sweetheart ❤️.....! |                  <<<< Give me one chance to prove my love ...! 🌹";
 let i = 0;
 const speed = 50;
 
 function typeWriter() {
     if (i < txt1.length) {
         if (txt1.charAt(i) == '<')
-            document.getElementById("text1").innerHTML += '</br>'
+            document.getElementById("text1").innerHTML += '</br>';
         else if (txt1.charAt(i) == '>')
-            document.getElementById("text1").innerHTML = ''
+            document.getElementById("text1").innerHTML = '';
         else
             document.getElementById("text1").innerHTML += txt1.charAt(i);
         i++;
